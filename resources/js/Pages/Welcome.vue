1289d954ac561596dfd0cb14407de80f4846470c
dhia nabli <dhianabli@outlook.com>
2023-08-07 02:37:18 +0200

    updated welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -51,7 +51,7 @@ defineProps({
 
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="flex justify-center">
-                <h1>Chatify</h1>
+                <h1>Chatify!</h1>
             </div>
 
             <div class="mt-16">
