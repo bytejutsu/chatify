09cd34c3a90ea3162ae563290b2ffed15c13245b
dhia nabli <dhianabli@outlook.com>
2023-08-07 03:15:16 +0200

    improved app title display

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -51,7 +51,7 @@ defineProps({
 
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="flex justify-center">
-                <h1>Chatify!</h1>
+                <h1 class="text-2xl">Chatify!</h1>
             </div>
 
             <div class="mt-16">
