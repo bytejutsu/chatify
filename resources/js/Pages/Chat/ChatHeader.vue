925e2cd2839dc4c1e52d4a05eb16066b26796ac4
dhia nabli <dhianabli@outlook.com>
2023-08-10 18:35:45 +0200

    improved chat ui

--- resources/js/Pages/Chat/ChatHeader.vue
+++ resources/js/Pages/Chat/ChatHeader.vue
@@ -1,17 +1,5 @@
 <template>
     <div class="flex items-center justify-between border-b p-2">
-        <!-- user info -->
-        <div class="flex items-center">
-            <img class="rounded-full w-10 h-10"
-                 src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
-            <div class="pl-2">
-                <div class="font-semibold">
-                    <a class="hover:underline" href="#">John Doe</a>
-                </div>
-                <div class="text-xs text-gray-600">Online</div>
-            </div>
-        </div>
-        <!-- end user info -->
         <!-- chat box action -->
         <div>
             <a class="inline-flex hover:bg-indigo-50 rounded-full p-2" href="#">
@@ -28,6 +16,18 @@
             </button>
         </div>
         <!-- end chat box action -->
+        <!-- user info -->
+        <div class="flex items-center">
+            <img class="rounded-full w-10 h-10"
+                 src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+            <div class="pl-2">
+                <div class="font-semibold">
+                    <a class="hover:underline" href="#">John Doe</a>
+                </div>
+                <div class="text-xs text-gray-600">Online</div>
+            </div>
+        </div>
+        <!-- end user info -->
     </div>
 </template>
 
--- resources/js/Pages/Chat/Index.vue
+++ resources/js/Pages/Chat/Index.vue
@@ -1,10 +1,20 @@
 <template>
     <Head title="Chat" />
 
-    <AuthenticatedLayout>
-        <div class="h-screen">
+    <AuthenticatedLayout class="max-h-screen">
+        <div class="flex flex-col bg-white h-[calc(100vh-65px)]">
             <ChatHeader />
-            <ChatInterface/>
+            <!--
+            <div class="flex flex-col grow overflow-y-scroll">
+                <span>hi</span>
+                <span>ho</span>
+                <span>ho</span>
+                <span>ho</span>
+                <span>ho</span>
+            </div>
+            -->
+            <ChatInterface class="flex flex-col grow overflow-y-scroll"/>
+            <ChatInput/>
         </div>
     </AuthenticatedLayout>
 </template>
@@ -14,6 +24,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import ChatInterface from '@/Pages/Chat/ChatInterface.vue';
 import ChatHeader from '@/Pages/Chat/ChatHeader.vue';
+import ChatInput from '@/Pages/Chat/ChatInput.vue';
 
 
 import { ref, onMounted } from 'vue';
