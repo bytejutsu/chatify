cd578fd62335c1328682d90f9e9ca981e86cc204
dhia nabli <dhianabli@outlook.com>
2023-08-11 21:02:36 +0200

    improved the chat ui

--- resources/js/Pages/Chat/ChatHeader.vue
+++ resources/js/Pages/Chat/ChatHeader.vue
@@ -1,21 +1,5 @@
 <template>
     <div class="flex items-center justify-between border-b p-2">
-        <!-- chat box action -->
-        <div>
-            <a class="inline-flex hover:bg-indigo-50 rounded-full p-2" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" />
-                </svg>
-            </a>
-
-            <button class="inline-flex hover:bg-indigo-50 rounded-full p-2" type="button">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-        </div>
-        <!-- end chat box action -->
         <!-- user info -->
         <div class="flex items-center">
             <img class="rounded-full w-10 h-10"
@@ -28,6 +12,16 @@
             </div>
         </div>
         <!-- end user info -->
+
+        <!-- chat box action -->
+        <div>
+            <button class="inline-flex hover:bg-indigo-50 rounded-full p-2" type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+        <!-- end chat box action -->
     </div>
 </template>
 
--- resources/js/Pages/Chat/Index.vue
+++ resources/js/Pages/Chat/Index.vue
@@ -2,20 +2,7 @@
     <Head title="Chat" />
 
     <AuthenticatedLayout class="max-h-screen">
-        <div class="flex flex-col bg-white h-[calc(100vh-65px)]">
-            <ChatHeader />
-            <!--
-            <div class="flex flex-col grow overflow-y-scroll">
-                <span>hi</span>
-                <span>ho</span>
-                <span>ho</span>
-                <span>ho</span>
-                <span>ho</span>
-            </div>
-            -->
-            <ChatInterface class="flex flex-col grow overflow-y-scroll"/>
-            <ChatInput/>
-        </div>
+        <ChatInterface/>
     </AuthenticatedLayout>
 </template>
 
@@ -23,9 +10,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import ChatInterface from '@/Pages/Chat/ChatInterface.vue';
-import ChatHeader from '@/Pages/Chat/ChatHeader.vue';
-import ChatInput from '@/Pages/Chat/ChatInput.vue';
-
 
 import { ref, onMounted } from 'vue';
 
