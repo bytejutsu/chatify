9327491dd4d9852bb3e84cf685f769af13638b4e
dhia nabli <dhianabli@outlook.com>
2023-09-02 14:40:53 +0200

    testing local commit

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -30,20 +30,20 @@ defineProps({
                 v-if="$page.props.auth.user"
                 :href="route('dashboard')"
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Dashboard</Link
-            >
+                >Dashboard
+            </Link>
             <template v-else>
                 <Link
                     :href="route('login')"
                     class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Log in</Link
-                >
+                    >Log in
+                </Link>
                 <Link
                     v-if="canRegister"
                     :href="route('register')"
                     class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Register</Link
-                >
+                    >Register
+                </Link>
             </template>
         </div>
         <div class="min-h-screen flex flex-col sm:justify-center items-center sm:pt-0">
@@ -55,4 +55,6 @@ defineProps({
         </div>
     </div>
 </template>
+
+
 
